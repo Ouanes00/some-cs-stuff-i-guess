ed7d6ea21af07cdc3e86ea497f76d56d93eb85d4
Ouanes._med <161247386+Ouanes00@users.noreply.github.com>
2024-04-30 16:39:39 +0100

    exo5files.c

--- exo5files.c
+++ exo5files.c
@@ -5,23 +5,16 @@ int main() {
   FILE *file;
   char data[100];
   time_t current_time;
-
-  file = fopen("log.txt", "a");  // Open in append mode to add new entries
-
+  file = fopen("randome_thoughts.txt", "a"); 
   if (file == NULL) {
-    printf("Error! Could not open the file.\n");
+    printf("error....couldn't create the file.\n");
     return 1;
-  }
-
-  time(&current_time);
-  printf("Enter your data: ");
-  fgets(data, 100, stdin);  // Read data including newline
-
-  fprintf(file, "%s - %s\n", ctime(&current_time), data);
-
-  fclose(file);
-
-  printf("Data logged successfully.\n");
+  }    time(&current_time);
+             printf("sooo wtvr wht do u wanna say :) : ");
+  fgets(data, 100, stdin);
+      fprintf(file, "%s - %s\n", ctime(&current_time), data);
+     fclose(file);
+      printf("Data entered succesfully bruh .");
 
   return 0;
 }
